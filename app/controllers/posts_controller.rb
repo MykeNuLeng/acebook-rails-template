2734898f9ef251b61224d1dbf3bd6d292fc72388
francescoFH <francescofacca@gmail.com>
2021-01-07 17:05:38 +0000

    User can edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,33 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully burnt in hell' }
-      format.json { head :no_content }
+    if @post.user != current_user
+      redirect_to posts_url, alert: "Don't be too devilish! You can only burn your own posts!"
+    else
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully burnt in hell' }
+        format.json { head :no_content }
+      end
     end
   end
 
   def index
     @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to posts_url, alert: "Don't be too devilish! You can only edit your own posts!"
+    else
+      @post.update(post_params)
+      redirect_to posts_url
+    end
   end
 
   private
